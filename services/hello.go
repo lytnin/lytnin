7fe3987e63aa16d9f50b1a519e4d8fae9ace2ef2
John K.E. Wilson <wilsonfiifi@gmail.com>
2018-10-01 18:51:42 +0000

    added hello service

---
+++ services/hello.go
@@ -0,0 +1,45 @@
+package services
+
+import (
+	"log"
+
+	_ "github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/source/file"
+	_ "github.com/lib/pq"
+	"github.com/lytnin/lytnin"
+	"github.com/urfave/cli"
+)
+
+// Hello service add a hello command to the main application
+type Hello struct {
+}
+
+// Info returns information about the service
+func (s *Hello) Info() interface{} {
+	return "Hello command"
+}
+
+// Init initializes the service and registers it with the application
+func (s *Hello) Init(a *lytnin.Application) {
+	a.AddCommand(s.Command())
+	a.AddService("hello", s)
+}
+
+// Close releases any resources used by the service
+func (s *Hello) Close() {
+}
+
+// Greet outputs a greeting to the console.
+func (s *Hello) Greet(ctx *cli.Context) error {
+	log.Println("Hello!")
+	return nil
+}
+
+// Command creates a cli command
+func (s *Hello) Command() cli.Command {
+	return cli.Command{
+		Name:   "hello",
+		Usage:  "greeting",
+		Action: s.Greet,
+	}
+}
